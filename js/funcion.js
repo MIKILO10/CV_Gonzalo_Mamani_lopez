a9ae115908e2098e1aa6b5f0e904471c142869a4
Gonzalo Mamaní López <gonzalomamani.ucse10@gmail.com>
2023-03-08 08:59:08 -0300

    Add: Nuevos Cambios

--- js/funcion.js
+++ js/funcion.js
@@ -1,16 +1,14 @@
-
- function irLinkedln(){
-    var Logo = document.getElementById("logo");
-    Logo.onclick = function (e) {
-        Swal.fire({
-           
-            icon: 'success',
-            title: 'Redireccionando',
-            showConfirmButton: false,
-            timer: 1200
-          })
-          setTimeout( function() { window.location.href = "https://www.linkedin.com/in/gjem10/"; }, 1202 );
-
- }}
-
- 
+function irLinkedln() {
+  var Logo = document.getElementById("logo");
+  Logo.onclick = function (e) {
+    Swal.fire({
+      icon: "success",
+      title: "Redireccionando",
+      showConfirmButton: false,
+      timer: 1200,
+    });
+    setTimeout(function () {
+      window.location.href = "https://www.linkedin.com/in/gjem10/";
+    }, 1202);
+  };
+}
